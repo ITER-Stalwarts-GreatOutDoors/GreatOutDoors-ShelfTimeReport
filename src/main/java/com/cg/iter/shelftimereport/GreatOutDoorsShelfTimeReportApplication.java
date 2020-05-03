b6bab59ebbd64e68ee21ed86f097339d558faef9
PlabanRoy <61134603+PlabanRoy@users.noreply.github.com>
2020-05-04 00:41:34 +0530

    First Commit

---
+++ src/main/java/com/cg/iter/shelftimereport/GreatOutDoorsShelfTimeReportApplication.java
@@ -0,0 +1,13 @@
+package com.cg.iter.shelftimereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatOutDoorsShelfTimeReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatOutDoorsShelfTimeReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/iter/shelftimereport/GreatOutDoorsShelfTimeReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.iter.shelftimereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatOutDoorsShelfTimeReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
